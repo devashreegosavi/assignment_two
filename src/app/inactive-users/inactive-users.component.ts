0ba28bdcf3bcc11c5f98e4365145e3e80ef76ad1
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-07-18 15:54:27 +0530

    18 july 2023

--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { UsersService } from '../users.service';
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
 export class InactiveUsersComponent {
+  users : string[];
+  constructor(private userService:UsersService){
 
+  }
+  ngOnInit(): void {
+    this.users=this.userService.inactiveUsers;
+  }
+
+  onSetToActive(id:number){
+    this.userService.setToActive(id);
+  }
 }
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -3,10 +3,12 @@ export class UsersService{
     inactiveUsers = ['Chris','Manu'];
 
     setToActive(id:number){
-
+        this.activeUsers.push(this.inactiveUsers[id]);
+        this.inactiveUsers.splice(id,1);
     }
     setToInactive(id:number){
-        
+        this.inactiveUsers.push(this.activeUsers[id]);
+        this.activeUsers.splice(id,1);
     }
 
 }
